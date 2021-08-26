6c457a1d199cb9621c17c71dfb2d9c6e70fb0b96
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2021-08-26 17:49:25 +0430

    extra file

---
+++ assets/js/logo.js
@@ -0,0 +1,9 @@
+document.getElementById("shop_logo").addEventListener("click", myFunction);
+function myFunction() {
+  window.location.href = "index.html";
+}
+
+// function myFunction() {
+//     alert('s');
+//     //window.location.href = "http://www.w3schools.com";
+// }
