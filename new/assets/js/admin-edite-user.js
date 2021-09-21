63c1ccf1f29676fab337e73fbc268ba50fd8bc28
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2021-09-22 01:27:55 +0330

    .

--- new/assets/js/admin-edite-user.js
+++
@@ -1,8 +0,0 @@
-function editeuser(){
-    var element = document.getElementById("edite-user-modal");
-    element.classList.add("show");
-}
-function closemodal(){
-   var element = document.getElementById("edite-user-modal");
-    element.classList.remove("show");  
-}
--- new/assets/js/logo.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("shop_logo").addEventListener("click", myFunction);
-function myFunction() {
-  window.location.href = "index.html";
-}
-
