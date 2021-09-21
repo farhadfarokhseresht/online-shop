e274222c6140997f962e682a9d219ba7b6117d28
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2021-09-22 01:22:16 +0330

    .

--- admin/admin-head.php
+++ admin/admin-head.php
@@ -28,55 +28,54 @@ include 'admin-app.php';
 </head>
 
 <body id="page-top">
-    <div id="wrapper">
-        <!-- Start: Sidebar -->
-        <nav class="navbar navbar-light align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" style="background: var(--bs-dark);">
-            <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0">
-                    <div class="sidebar-brand-icon"><i class="fas fa-user-tie"></i></div>
-                    <div class="sidebar-brand-text mx-3"><span>پنل مدیریت</span></div>
-                </a>
-                <hr class="sidebar-divider my-0">
-                <ul class="navbar-nav text-light" id="accordionSidebar">
-                    <li class="nav-item"><a class="nav-link" href="admin.php"><i class="fas fa-poll"></i><span>داشبرد</span></a></li>
-                    <hr class="sidebar-divider">
-                    <div class="sidebar-heading">
-                        <p class="mb-0">&nbsp;مدیریت محصولات&nbsp;</p>
-                    </div>
-                    <li class="nav-item">
-                        <div><a class="btn btn-link nav-link" data-bs-toggle="collapse" aria-expanded="false" aria-controls="collapse" href="#collapse" role="button"><i class="fas fa-boxes"></i>&nbsp;<span>لیست محصولات</span></a>
-                            <div class="collapse" id="collapse">
-                                <div class="bg-white border rounded py-2 collapse-inner" style="overflow: auto;max-height: 200px;position: relative;"><i class="fas fa-angle-double-down flash animated infinite" style="position: absolute;color: var(--bs-blue);bottom: 10%;left: 10%;"></i>
-                                    <h6 class="collapse-header">کالای دیجیتال</h6><a class="collapse-item" href="admin-product-list.php">مبایل</a><a class="collapse-item" href="admin-product-list.php">لبتاب</a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="admin-add-product.php"><i class="fas fa-plus-circle"></i><span>افزودن کالای جدید</span></a><a class="nav-link" href="#"><i class="fas fa-donate"></i><span>تخفیفات</span></a></li>
-                    <hr class="sidebar-divider">
-                    <div class="sidebar-heading">
-                        <p class="mb-0">مدیریت کاربران&nbsp;</p>
-                    </div>
-                    <li class="nav-item"><a class="nav-link" href="admin-add-user.php"><i class="fas fa-user-plus"></i><span>افزودن کاربر</span></a><a class="nav-link" href="admin-user-list.php"><i class="fas fa-users"></i><span>لیست کاربران</span></a></li>
-                    <hr class="sidebar-divider">
-                    <div class="sidebar-heading">
-                        <p class="mb-0">مدیریت فروش&nbsp;</p>
-                    </div>
-                    <li class="nav-item"><a class="nav-link" href="admin-order-list.php"><i class="fas fa-clipboard-list"></i><span>سفارشات</span></a></li>
-                    <hr class="sidebar-divider">
-                </ul>
-                <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
-            </div>
-        </nav>
-        <!-- End: Sidebar -->
-
-        <div class="d-flex flex-column" id="content-wrapper">
-            <div id="content">
-                <nav class="navbar navbar-light navbar-expand bg-white d-flex justify-content-center align-items-center mb-4 topbar static-top" style="direction: rtl;">
-                    <div class="container-fluid">
-                        <ul class="navbar-nav" style="padding-right: 0px;">
-                            <li class="nav-item d-flex"><a class="nav-link"><span>خروج</span><i class="fas fa-share" style="margin-right: 5px;"></i></a></li>
-                            <div class="d-none d-sm-block topbar-divider"></div>
-                        </ul>
-                        <div><img id="shop_logo" src="../assets/img/13e7bddc-3c00-45ff-b8a4-84901b6e9b80.png"></div>
-                    </div>
-                </nav>
+<div id="wrapper">
+	<!-- Start: Sidebar -->
+	<nav class="navbar navbar-light align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" style="background: var(--bs-dark);">
+		<div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0">
+				<div class="sidebar-brand-icon"><i class="fas fa-user-tie"></i></div>
+				<div class="sidebar-brand-text mx-3"><span>پنل مدیریت</span></div>
+			</a>
+			<hr class="sidebar-divider my-0">
+			<ul class="navbar-nav text-light" id="accordionSidebar">
+				<li class="nav-item"><a class="nav-link" href="admin.html"><i class="fas fa-poll"></i><span>داشبرد</span></a><a class="nav-link" href="admin.html"><i class="fas fa-file-invoice"></i><span>تبلیغات صفحه اصلی</span></a></li>
+				<hr class="sidebar-divider">
+				<div class="sidebar-heading">
+					<p class="mb-0">&nbsp;مدیریت محصولات&nbsp;</p>
+				</div>
+				<li class="nav-item">
+					<div><a class="btn btn-link nav-link" data-bs-toggle="collapse" aria-expanded="false" aria-controls="collapse" href="#collapse" role="button"><i class="fas fa-boxes"></i>&nbsp;<span>لیست محصولات</span></a>
+						<div class="collapse" id="collapse">
+							<div class="bg-white border rounded py-2 collapse-inner" style="overflow: auto;max-height: 200px;position: relative;"><i class="fas fa-angle-double-down flash animated infinite" style="position: absolute;color: var(--bs-blue);bottom: 10%;left: 10%;"></i>
+								<h6 class="collapse-header">کالای دیجیتال</h6><a class="collapse-item" href="admin-product-list.html">مبایل</a><a class="collapse-item" href="admin-product-list.html">لبتاب</a>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li class="nav-item"><a class="nav-link" href="admin-add-product.html"><i class="fas fa-plus-circle"></i><span>افزودن کالای جدید</span></a></li>
+				<li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-donate"></i><span>تخفیفات</span></a></li>
+				<hr class="sidebar-divider">
+				<div class="sidebar-heading">
+					<p class="mb-0">مدیریت کاربران&nbsp;</p>
+				</div>
+				<li class="nav-item"><a class="nav-link" href="admin-user-list.html"><i class="fas fa-users"></i><span>لیست کاربران</span></a><a class="nav-link" href="admin-user-list.html"><i class="fas fa-sms"></i><span>ارسال پیامک به کاربران</span></a></li>
+				<hr class="sidebar-divider">
+				<div class="sidebar-heading">
+					<p class="mb-0">مدیریت فروش&nbsp;</p>
+				</div>
+				<li class="nav-item"><a class="nav-link" href="admin-order-list.html"><i class="fas fa-clipboard-list"></i><span>سفارشات</span></a></li>
+				<hr class="sidebar-divider">
+			</ul>
+			<div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
+		</div>
+	</nav><!-- End: Sidebar -->
+	<div class="d-flex flex-column" id="content-wrapper">
+		<div id="content">
+			<nav class="navbar navbar-light navbar-expand bg-white d-flex justify-content-center align-items-center mb-4 topbar static-top" style="direction: rtl;">
+				<div class="container-fluid">
+					<ul class="navbar-nav" style="padding-right: 0px;">
+						<li class="nav-item d-flex"><a class="nav-link"><span>خروج</span><i class="fas fa-share" style="margin-right: 5px;"></i></a></li>
+						<div class="d-none d-sm-block topbar-divider"></div>
+					</ul>
+					<div><img id="shop_logo" src="../assets/img/13e7bddc-3c00-45ff-b8a4-84901b6e9b80.png"></div>
+				</div>
+			</nav><!-- Start: dash users list -->
