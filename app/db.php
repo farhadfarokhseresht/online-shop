7761528ed29bf8dbc831c0fd7e3cdc426d829aee
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2021-11-02 13:27:53 +0330

    set db

--- app/db.php
+++ app/db.php
@@ -1,12 +1,18 @@
 <?php
 
-$serverfname = "localhost";
-$userfname = "root";
-$password = "";
+//$serverfname = "localhost";
+//$userfname = "root";
+//$password = "";
+//$db = "onlineshop";
+//
+$serverfname = "222.222.222.22";
+$userfname = "vistauser";
+$password = "farhadf72";
+$port = '3306';
 $db = "onlineshop";
 
 // Create connection
-$con = mysqli_connect($serverfname, $userfname, $password,$db);
+$con = mysqli_connect($serverfname, $userfname, $password,$db,$port);
 $con->set_charset("utf8");
 // Check connection
 if (!$con) {
